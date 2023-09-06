aedaf871f4d82cdb7d87af3126fe6bfef79dd938
baroonjha <85762785+baroonjha@users.noreply.github.com>
2023-09-06 20:31:25 +0530

    created till login and register page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Login from './pages/Login'
+// import Product from './pages/Product'
+import Register from './pages/Register'
+// import Home from './pages/Home'
+// import ProductList from './pages/ProductList'
+
+
+function App() {
+
+  return (
+    <>
+     <Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Product.jsx
@@ -0,0 +1,159 @@
+//single product page
+
+import styled from "styled-components"
+import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement"
+import Footer from "../components/Footer"
+import Newsletter from "../components/Newsletter"
+import { Add, Remove } from "@mui/icons-material"
+
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    padding: 50px;
+`;
+const ImgContainer = styled.div`
+    flex:1;
+`;
+const Image = styled.img`
+width: 100%;
+height: 90vh;
+object-fit: cover;
+`;
+const InfoContainer = styled.div`
+ flex:1;
+ padding: 0px 50px;
+`;
+const Title = styled.h1`
+    font-weight: 200;
+`;
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+const Price = styled.span`
+font-weight:100;
+font-size: 48px;
+`;
+
+const FilterContainer = styled.div`
+width: 50%;
+display: flex;
+justify-content: space-between;
+margin: 30px 0px;
+`;
+
+const Filter = styled.div`
+display: flex;
+align-items: center;
+`;
+
+const FilterTitle = styled.span`
+font-size: 20px;
+font-weight: 400;
+`;
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+    margin: 0px 3px;
+    cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+ margin-left: 5px;
+ padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+display: flex;
+align-items: center;
+font-weight: 700;
+font-size: 20px;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border:1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin:0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    border: 2px solid teal;
+    background-color: white;
+    color: black;
+    font-weight: 500;
+    cursor:pointer;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+
+`;
+
+
+
+
+const Product = () => {
+  return (
+    <Container>
+        <Navbar />
+        <Announcement/>
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Denim Jeans</Title>
+                <Desc>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic cumque esse architecto placeat unde voluptatum quos commodi eligendi molestias numquam.</Desc>
+                <Price> $ 20</Price>
+                <FilterContainer>
+                    <Filter>
+                    <FilterTitle>Color</FilterTitle>
+                    <FilterColor color="red"/>
+                    <FilterColor color="black"/>
+                    <FilterColor color="blue"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                        <FilterSizeOption>XS</FilterSizeOption>
+                        <FilterSizeOption>S</FilterSizeOption><FilterSizeOption>M</FilterSizeOption><FilterSizeOption>L</FilterSizeOption><FilterSizeOption>XL</FilterSizeOption>
+                        <FilterSizeOption>XXL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <Remove style={{}}/>
+                        <Amount>1</Amount>
+                        <Add />
+                    </AmountContainer>
+                    <Button>Add to Cart</Button>
+                </AddContainer>
+            </InfoContainer>
+        </Wrapper>
+        <Newsletter />
+        <Footer />
+    </Container>
+  )
+}
+
+export default Product
